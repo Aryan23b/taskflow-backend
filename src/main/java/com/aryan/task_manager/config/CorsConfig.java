bc83979c2bb0b33e97306c19283278a3e38266e7
Aryan23b <baranwalaryan2520@gmail.com>
2026-09-22 16:59:23 +0530

    resolve the errors

--- src/main/java/com/aryan/task_manager/config/CorsConfig.java
+++ src/main/java/com/aryan/task_manager/config/CorsConfig.java
@@ -35,6 +35,7 @@ public class CorsConfig implements WebMvcConfigurer {
                         "GET",
                         "POST",
                         "PUT",
+                        "PATCH",
                         "DELETE",
                         "OPTIONS"
                 )
--- src/main/java/com/aryan/task_manager/security/SecurityConfig.java
+++ src/main/java/com/aryan/task_manager/security/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -55,6 +56,8 @@ public class SecurityConfig {
 
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+
+                .cors(Customizer.withDefaults())
 
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(
